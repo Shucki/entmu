51137eeb91a7c889d214d6dea761bed67950a99f
Jan Faessler <mail@janfaessler.ch>
2012-10-17 11:13:31 +0200

    adding package information

---
+++ src/jdraw/figures/handles/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2000-2012 Fachhochschule Nordwestschweiz (FHNW)
+ * All Rights Reserved. 
+ */
+
+/**
+ * Contains all implementations of figure handles for
+ * manipulating the figures.
+ * 
+ * @author Jan Faessler
+ */
+package jdraw.figures.handles;
---
+++ src/jdraw/figures/tools/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2000-2012 Fachhochschule Nordwestschweiz (FHNW)
+ * All Rights Reserved. 
+ */
+
+/**
+ * Contains all implementations of figure-tools that provide the
+ * figures
+ * 
+ * @author Jan Faessler
+ */
+package jdraw.figures.tools;
